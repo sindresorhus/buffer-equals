0dded6cdf64e8556dd8e424027545c3ba57eda73
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-01 14:26:33 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function (a, b) {
+	if (!(a instanceof Buffer && b instanceof Buffer)) {
+		throw new TypeError('Argument must be a Buffer');
+	}
+
+	if (typeof a.equals === 'function') {
+		return a.equals(b);
+	}
+
+	if (a.length !== b.length) {
+		return false;
+	}
+
+	for (var i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) {
+			return false;
+		}
+	}
+
+	return true;
+};
---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+var assert = require('assert');
+var test = require('ava');
+var bufferEquals = require('./');
+
+test(function (t) {
+	var a = new Buffer('abcdf');
+	var b = new Buffer('abcdf');
+	var c = new Buffer('abcde');
+	var d = new Buffer('abcdef');
+
+	t.assert(bufferEquals(a, b));
+	t.assert(!bufferEquals(b, c));
+	t.assert(!bufferEquals(c, d));
+
+	t.assert(!bufferEquals(new Buffer('abc'), new Buffer('a0c')));
+
+	assert.throws(function() {
+		bufferEquals(new Buffer(1), 'abc');
+	});
+
+	t.end();
+});
